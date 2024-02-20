65ed07235bb3bf5b211040a053f7240d9a4d188a
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-02-20 18:42:10 +0100

    added schedule of second snack

--- script.php
+++ script.php
@@ -1,5 +1,16 @@
 <?php
 
+//TODO COSE DA FARE - SCALETTA
+//~ 1. Raccogliere gli elementi con il metodo GET
+//~ 2. Validare l'età assicurandosi che sia maggiore di zero e che venga effetivamente passata come un numero e non come una stringa tramite la funzione is_numeric.
+//~ 3. Validare il nome assicurandosi che sia più lungo di 3 caratteri, verificando anche la lunghezza tramite mb_strlen, nel caso vengano inseriti caratteri diversi -
+//~ - che occupano più byte, e il trim per evitare che eventuali spazi influiscano nella conta dei caratteri.
+//~ 4. Validare la mail assicurandosi che contenga un '.' e una '@' tramite la funzione str_contains.
+//~ 5. Creare una variabile per confermare se tutti e tre i campi richiesti sono stati riempiti.
+//~ 6. Stampare il messaggio di 'Accesso Riuscito' in caso di successo oppure 'Accesso Negato' in caso di fallimento.
+
+
+
 ?>
 
 
